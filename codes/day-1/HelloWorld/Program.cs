4e330e6650a81b68e42472cd60927269dc8091ff
Joydip <joydip.mondal@gmail.com>
2025-05-26 12:25:00 +0530

    basics upload

---
+++ codes/day-1/HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        //static void Main(string[] args)
+        //static void Main()
+        //static int Main(string[] args)
+        //static int Main()
+        static void Main()
+        {
+            //int x = 10;
+            //Int32 y = 20;
+            Console.WriteLine("Hello, World!");
+            SayHi();
+            SayHi();
+            Console.WriteLine("press any key to close the app...");
+            Console.ReadKey();
+        }
+        static void SayHi()
+        {
+            Console.WriteLine("Hi...");
+        }
+    }
+}
