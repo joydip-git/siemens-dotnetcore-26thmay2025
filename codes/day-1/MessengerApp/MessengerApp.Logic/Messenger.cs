660a28581bbf023673d1509c2e68546647705153
Joydip <joydip.mondal@gmail.com>
2025-05-26 14:26:55 +0530

    task updated

---
+++ codes/day-1/MessengerApp/MessengerApp.Logic/Messenger.cs
@@ -0,0 +1,13 @@
+﻿namespace MessengerApp.Logic
+{
+    public class Messenger
+    {
+        //public string GetMessage(string name)
+        //{
+        //    return "welcome " + name;
+        //}
+
+        //Expression body syntax
+        public string GetMessage(string name) => $"welcome {name}";
+    }
+}
---
+++ codes/day-1/MessengerApp/MessengerApp.UserInterface/Program.cs
@@ -0,0 +1,15 @@
+﻿using MessengerApp.Logic;
+
+namespace MessengerApp.UserInterface
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //Messenger messenger = new Messenger();
+            Messenger messenger = new();
+            string message = messenger.GetMessage("joydip");
+            Console.WriteLine($"Message: {message}");
+        }
+    }
+}
