bb892f68089d5f997280958e7720aad59160bb71
Joydip <joydip.mondal@gmail.com>
2025-05-28 14:09:10 +0530

    delegate basics

---
+++ codes/day-3/CollectionOperations/Program.cs
@@ -0,0 +1,27 @@
+﻿//1. create a list to store integers
+//2. create static local function (Filter) which can be called to get the filtered numbers from a list
+//3. call the method, pass the list created in the 1st step
+//4. the method MUST return another list with just the filtered numbers
+//5. print the filtered numbers
+//filter the even numbers from the list
+
+List<int> numbers = [1, 3, 5, 2, 6, 4, 8, 9, 7, 0];
+List<int> result = Filter(numbers);
+foreach (int number in result)
+{
+    Console.WriteLine(number);
+}
+
+static List<int> Filter(List<int> input)
+{
+    List<int> output = [];
+    foreach (int item in input)
+    {
+        if (item % 2 == 0)
+            output.Add(item);
+    }
+    return output;
+}
+
+static bool IsEven(int value) => value % 2 == 0;
+static bool IsOdd(int value) => value % 2 != 0;
---
+++ codes/day-3/DelegateDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+CalDel cdAdd = new CalDel(Add);
+
+Calculation calc = new();
+CalDel cdSub = new CalDel(calc.Subtract);
+
+CalDel cdMulti = Calculation.Multiply;
+
+//cdAdd(12, 3);
+//cdSub.Invoke(12, 3);
+//cdMulti(12,3);
+InvokeMethod(cdAdd);
+InvokeMethod(cdSub);
+InvokeMethod(cdMulti);
+
+static void InvokeMethod(CalDel cd)
+{
+    cd(12,3);
+}
+static void Add(int x, int y) => Console.WriteLine(x + y);
+
+delegate void CalDel(int a, int b);
+
+class Calculation
+{
+    public void Subtract(int a, int b) => Console.WriteLine(a - b);
+
+    public static void Multiply(int a, int b) => Console.WriteLine(a * b);
+}
